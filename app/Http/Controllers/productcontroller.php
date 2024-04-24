9f62a6312cc7b4b5f464673e93e239bf4e91d3a7
abdullahraihan03 <aaziz191231@bscse.uiu.ac.bd>
2024-04-24 17:07:32 +0600

    added todays(4-24-24)codes

---
+++ app/Http/Controllers/productcontroller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class productcontroller extends Controller
+{
+    public function index(){
+
+    }
+    public function create(){
+        return view('products.create');
+    }
+    public function store(){
+        
+    }
+    public function edit(){
+        
+    }
+    public function update(){
+        
+    }
+    // this methood will show product delete
+    public function destroy(){
+        
+    }
+}
