613e705dc893cfcb9b450e7705b3b11b8b991c97
abdullahraihan03 <aaziz191231@bscse.uiu.ac.bd>
2024-05-08 12:10:45 +0600

    added refreash codes

--- routes/web.php
+++ routes/web.php
@@ -15,39 +15,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/products',[ProductController::class,'index'])->name('products.index');
-
-// Route::get('/products/create',[ProductController::class,'create']) ->name('products.create');
-
-// Route::post('/products',[ProductController::class,'store'])->name('products.store');
-
-// Route::get('/products/{product}/edit',[ProductController::class,'edit'])->name('products.edit');
-
-// Route::put('/products/{product}',[ProductController::class,'update'])->nam('products.update');
-// Route::delete('/products/{product}',[ProductController::class,'destroy'])->nam('products.destroy');
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::Controller(ProductController::class,)->group(function(){
+Route::get('/products',[ProductController::class,'index'])->name('products.index');
 
-    Route::get('/products','index')->name('products.index');
+Route::get('/products/create',[ProductController::class,'create']) ->name('products.create');
 
-    Route::get('/products/create','create') ->name('products.create');
-    
-    Route::post('/products','store')->name('products.store');
-    
-    Route::get('/products/{product}/edit','edit')->name('products.edit');
-    
-    Route::put('/products/{product}','update')->nam('products.update');
-    Route::delete('/products/{product}','destroy')->name('products.destroy');
+Route::post('/products',[ProductController::class,'store'])->name('products.store');
 
-});
+Route::get('/products/{product}/edit',[ProductController::class,'edit'])->name('products.edit');
+
+Route::put('/products/{product}',[ProductController::class,'update'])->nam('products.update');
+Route::delete('/products/{product}',[ProductController::class,'destroy'])->nam('products.destroy');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::Controller(ProductController::class,)->group(function(){
+
+//     Route::get('/products','index')->name('products.index');
+
+//     Route::get('/products/create','create') ->name('products.create');
+    
+//     Route::post('/products','store')->name('products.store');
+    
+//     Route::get('/products/{product}/edit','edit')->name('products.edit');
+    
+//     Route::put('/products/{product}','update')->nam('products.update');
+//     Route::delete('/products/{product}','destroy')->name('products.destroy');
+
+// });
 
 
 // Route::get('/products/list',[ProductController::class,'list']) ->name('products.list'); 
